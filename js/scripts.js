1bcfc8f02a8a93f2e1b54aa78a2581f1f98b589c
aregnante2021 <66320348+aregnante2021@users.noreply.github.com>
2021-03-15 11:08:13 -0400

    fixing body w/navbar

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,21 @@ window.onscroll = function() {stickyNav()};
 //Get the navbar
 var navbar = document.getElementById("navbar");
 
+//Get the main content
+var mainText = document.getElementById("main-text");
+
 //Get the offset position of the navbar
-var sticky = navbar.offsetTop + 335;
+var sticky = navbar.offsetTop + 339;
 
 //Add the sticky class to the navbar when you reach its scroll position
 //Remove the sticky class when you leave the scroll position
 function stickyNav() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky")
+        mainText.classList.add("top-bumper")
     } else {
         navbar.classList.remove("sticky")
+        mainText.classList.remove("top-bumper")
     }
 }
 
