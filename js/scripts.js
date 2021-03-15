624c64e19aa8d8e7f38553a96a49b1ec0e9516cd
aregnante2021 <66320348+aregnante2021@users.noreply.github.com>
2021-03-15 10:13:57 -0400

    sticky navbar??????????

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
+//JS for sticky navbar
+//When the user scrolls on the page, execute stickyNav
+window.onscroll = function() {stickyNav()};
+
+//Get the navbar
+var navbar = document.getElementById("navbar");
+
+//Get the offset position of the navbar
+var sticky = navbar.offsetTop + 335;
+
+//Add the sticky class to the navbar when you reach its scroll position
+//Remove the sticky class when you leave the scroll position
+function stickyNav() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky")
+    }
+}
+
 document.getElementsByClassName("nav-item").style.color = "blue";
