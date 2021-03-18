7ba185f9643bc9dd02e99654ad6a145433aafadb
aregnante2021 <66320348+aregnante2021@users.noreply.github.com>
2021-03-18 11:14:15 -0400

    big homepage?

--- js/navbar.js
+++ js/navbar.js
@@ -4,12 +4,14 @@ window.onscroll = function() {stickyNav()};
 
 //Get the navbar
 var navbar = document.getElementById("navbar");
+var navbarHome = document.getElementById("navbar-home");
 
 //Get the main content
 var mainText = document.getElementById("main-text");
 
 //Get the offset position of the navbar
-var sticky = navbar.offsetTop + 339;
+var sticky = navbar.offsetTop + 285;
+var stickyHome = navbarHome.offsetTop;
 
 //Add the sticky class to the navbar when you reach its scroll position
 //Remove the sticky class when you leave the scroll position
@@ -23,4 +25,13 @@ function stickyNav() {
     }
 }
 
-//JS for navbar color change
+function stickyHomeNav() {
+    if (window.pageYOffset >= stickyHome) {
+        navbarHome.classList.add("sticky")
+        mainText.classList.add("top-bumper")
+    } else {
+        navbarHome.classList.remove("sticky")
+        mainText.classList.remove("top-bumper")
+    }
+}
+
